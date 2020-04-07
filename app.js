2887afbea2ab2000ddab46b647b17a8ef5bd4961
Stavros Vergos <stavrospvergos@gmail.com>
2020-04-07 13:09:23 +0300

    initial commit

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ Game rules:
 */
 
 // Game values
-let min = 1,
-    max = 10,
+let min = 2,
+    max = 15,
     winningNum = 2,
     guessesLeft = 3;
 
@@ -17,8 +17,38 @@ let min = 1,
 const game = document.getElementById('game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
-      guessBtn = document.querySelector('.#guess-btn'),
-      guessInput = document.querySelector('.#guess-input'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
       message = document.querySelector('.message');
 
-      
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+
+    // Validate
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+    // Check if won
+    if(guess === winningNum){
+        // Disable input
+        guessInput.disabled = true;
+        // Change border color
+        guessInput.style.borderColor = 'green';
+        // Set message
+        setMessage(`${winningNum} is correct! Hooray!`, 'green');
+    } else {
+
+    }
+});
+
+// Set message
+function setMessage(msg, color){
+    message.style.color = color;
+    message.textContent = msg;
+}
